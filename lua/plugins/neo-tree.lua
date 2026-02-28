f9c21a064d99d33e8bf5538722c69fbcd22fd4d1
mathoek <matthijs_hoekstra@outlook.com>
2026-02-28 11:59:35 +0100

    Added extendend git functionality

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -147,6 +147,30 @@ return {
           highlight = "NeoTreeFileIcon"
         },
         git_status = {
+	    window = {
+		position = "float",
+		mappings = {
+		    ["A"] = "git_add_all",
+		    ["gu"] = "git_unstage_file",
+		    ["gU"] = "git_undo_last_commit",
+		    ["ga"] = "git_add_file",
+		    ["gt"] = "git_toggle_file_stage",
+		    ["gr"] = "git_revert_file",
+		    ["gc"] = "git_commit",
+		    ["gp"] = "git_push",
+		    ["gg"] = "git_commit_and_push",
+		    ["o"] = {
+			"show_help",
+			nowait = false,
+			config = { title = "Order by", prefix_key = "o" },
+		    },
+		    ["oc"] = { "order_by_created", nowait = false },
+		    ["od"] = { "order_by_diagnostics", nowait = false },
+		    ["om"] = { "order_by_modified", nowait = false },
+		    ["on"] = { "order_by_name", nowait = false },
+		    ["os"] = { "order_by_size", nowait = false },
+		    ["ot"] = { "order_by_type", nowait = false },
+		},
           symbols = {
             added     = "✚",
             modified  = "",
@@ -157,9 +181,9 @@ return {
             unstaged  = "󰄱",
             staged    = "",
             conflict  = "",
-          }
+          },
         },
       },
     },
   }
-}
+}}
