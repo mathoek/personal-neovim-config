24ae07a597fd8f1cde506e7f5528252529835d1b
mathoek <matthijs.hoekstra1905@gmail.com>
2026-03-01 18:48:43 +0100

    Moved some compatibility stuff in init.lua

--- init.lua
+++ init.lua
@@ -7,20 +7,15 @@ vim.opt.clipboard = "unnamedplus"
 local is_windows = vim.loop.os_uname().version:match("Windows")
 if is_windows then
     -- Windows specific settings
+    local shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
     vim.opt.shell = "powershell.exe"
+    vim.opt.shell = shell
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+    vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
 else
     vim.g.clipboard = "xclip"
 end
 
--- =====================================================================
--- PowerShell Integration Fix for Windows
--- =====================================================================
--- Check if PowerShell Core (pwsh) is installed, otherwise use standard powershell
-local shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
-
-vim.opt.shell = shell
-vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.opt.shellquote = ""
-vim.opt.shellxquote = ""
